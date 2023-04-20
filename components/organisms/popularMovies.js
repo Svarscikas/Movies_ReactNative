5389f5455f3703b59c8c78bf22a5cb3196164429
Arnas <arnas.e@gmail.com>
2023-04-20 14:48:00 +0300

    added some error handling

--- components/organisms/popularMovies.js
+++ components/organisms/popularMovies.js
@@ -6,74 +6,96 @@ import { useNavigation } from '@react-navigation/native';
 
 export default function PopularMovies({type, id}) {
     const [popularMovies, setPopularMovies] = useState([]);
+    const [error, setError] = useState("");
     const navigation = useNavigation();
     const [isLoading, setLoading] = useState(true);
 
 
     const getPopularMovies = async () => {
         try {
+            var json;
             if (type == "popular") {
-                const response = await fetch( constants.Base_URL + '3/movie/popular?api_key=' + constants.API_Key + '&language=en-US&page=1');
-                const json = await response.json();
-                setPopularMovies(json.results);
-                
+                const response = await fetch( constants.Base_URL + '3/movie/popular?api_key=' +constants.API_Key + '&language=en-US&page=1');
+                json = await response.json();
+                if(json.status_message){
+                    setError(json.status_message);
+                } 
             }
             if(type == "similiar") {
                 const movieId = id;
                 const response = await fetch('https://api.themoviedb.org/3/movie/'+ id +'/similar?api_key='+ constants.API_Key+ '&language=en-US&page=1');
-                const json = await response.json();
-                setPopularMovies(json.results);
+                json = await response.json();
+                if(json.status_message){
+                    setError(json.status_message);
+                }
             }
             if(type == "latest") {
                 const response = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=0839f265b7b8e52c2ec1711623246e99&language=en-US&page=1');
-                const json = await response.json();
-                setPopularMovies(json.results);
+                json = await response.json();
+                if(json.status_message){
+                    setError(json.status_message);
+                }
             }   
             if(type == "topRated") {
                 const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=0839f265b7b8e52c2ec1711623246e99&language=en-US&page=1');
-                const json = await response.json();
-                setPopularMovies(json.results);
+                json = await response.json();
+                if(json.status_message){
+                    setError(json.status_message);
+                }
             }
         } catch (error) {
-          console.error(error);
+          setError(error);
         } finally {
+            
           setLoading(false);
+          setPopularMovies(json.results);
         }
     };
     useEffect(() => {
         getPopularMovies();
     }, [id]);
-
-    return (
-        <View style={{paddingBottom: 10, alignContent:"center", paddingTop: 10, alignItems: "center", justifyContent: "center"}}>
-                    {isLoading ? (
-                    <ActivityIndicator />
-                    ) : (
-                    <FlatList
-                        data={popularMovies}
-                        showsHorizontalScrollIndicator= {false}
-                        horizontal={true}
-                        keyExtractor={({id})=> id}
-                        renderItem={({item}) => (
-                            <View style={{flex: 1, alignItems:"center", justifyContent: "center", padding:5}}>
-                                <TouchableOpacity onPress={() => navigation.navigate("Details", {
-                                        title: item.title,
-                                        description: item.overview,
-                                        movieId: item.id,
-                                    })}>
-                                    <MyImage imageSource={item.backdrop_path}></MyImage>
-                                    <Text style={{textAlign:"center", fontSize:16}}>
-                                        {item.title}
-                                    </Text>
-                                </TouchableOpacity>
+    if(error) {
+        return(
+            <View style={{flex:1, justifyContent:"center"}}>
+                <Text style={{fontSize: 16, textAlign:"center"}}>
+                    {error}
+                </Text>
+            </View>
+        )
+    }
+    else{
+        return (
+            <View style={{paddingBottom: 10, alignContent:"center", paddingTop: 10, alignItems: "center", justifyContent: "center"}}>
+                        {isLoading ? (
+                        <ActivityIndicator />
+                        ) : (
+                        <FlatList
+                            data={popularMovies}
+                            showsHorizontalScrollIndicator= {false}
+                            horizontal={true}
+                            keyExtractor={({id})=> id}
+                            renderItem={({item}) => (
+                                <View style={{flex: 1, alignItems:"center", justifyContent: "center", padding:5}}>
+                                    <TouchableOpacity onPress={() => navigation.navigate("Details", {
+                                            title: item.title,
+                                            description: item.overview,
+                                            movieId: item.id,
+                                        })}>
+                                        <MyImage imageSource={item.backdrop_path}></MyImage>
+                                        <Text style={{textAlign:"center", fontSize:16}}>
+                                            {item.title}
+                                        </Text>
+                                    </TouchableOpacity>
+                                    
+                                    
+                                </View>
                                 
-                                
-                            </View>
-                            
-                        )}   
-                    >
-                    </FlatList>
-                    )}
-        </View>
-    )
+                            )}   
+                        >
+                        </FlatList>
+                        )}
+            </View>
+        )
+    }
+   
 }
--- screens/PlayerScreen.js
+++ screens/PlayerScreen.js
@@ -6,31 +6,43 @@ import constants from "../constants";
 export default function PlayerScreen({route}) {
     
     const [videoLink, setVideoLink] = useState("");
-
+    const [error, setError] = useState("");
     const {id} = route.params;
     const getData = async ()=> {
         try {
             //console.log('https://api.themoviedb.org/3/movie/'+ id +'/videos?api_key='+ constants.API_Key + '&language=en-US')
             const response = await fetch('https://api.themoviedb.org/3/movie/'+ id +'/videos?api_key='+ constants.API_Key + '&language=en-US')
-            const json = await response.json();
-            setVideoLink(json.results[0].key);
+            var json = await response.json();
+            if(json.status_message){
+                setError(json.status_message);
+            }
         }
         catch(error) {
-            console.log(error);
+            setError(error);
         }
-        finally {
-
+        finally{
+            setVideoLink(json.results[0].key);
         }
     }
     useEffect(() =>{
         getData()
     },[])
-    return(
-        <View style={{flex: 1}}>
-            <WebView
-                javaScriptEnabled={true}
-                source={{uri: 'https://www.youtube.com/embed/'+ videoLink}}
-                />
-        </View>
-    )
+    if(error){
+        return (
+            <View style={{flex:1,justifyContent:"center"}}>
+                <Text style={{fontSize:16, textAlign:"center"}}>{error}</Text>
+            </View>
+        )
+    }
+    else{
+        return(
+            <View style={{flex: 1}}>
+                <WebView
+                    javaScriptEnabled={true}
+                    source={{uri: 'https://www.youtube.com/embed/'+ videoLink}}
+                    />
+            </View>
+        )
+    }
+    
 }
